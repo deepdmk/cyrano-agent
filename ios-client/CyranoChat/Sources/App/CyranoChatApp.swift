c1aa0e422f1e7cdd17b3efa0b9f0717b787eda5a
Richard Amerman <richard@amerman.net>
2026-02-28 12:01:39 -0800

    feat: Eagerly initialize model managers to copy bundled models on first launch.

--- ios-client/CyranoChat/Sources/App/CyranoChatApp.swift
+++ ios-client/CyranoChat/Sources/App/CyranoChatApp.swift
@@ -4,6 +4,13 @@ import SwiftUI
 
 @main
 struct CyranoChatApp: App {
+    init() {
+        // Eagerly initialize model managers so they copy bundled models
+        // to Documents on first launch
+        _ = GLMASRModelManager.shared
+        _ = OnDeviceLLMModelManager.shared
+    }
+
     var body: some Scene {
         WindowGroup {
             ChatView()
